85201b5c1df820e7cad6cc8efb90beac199ef8e0
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-04-08 15:48:47 +0700

    first commit

---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import NewsLists from '@/Components/Homepage/NewsLists';
+import Paginator from '@/Components/Homepage/Paginator';
+import Navbar from '@/Components/Navbar';
+import { Link, Head } from '@inertiajs/react';
+
+export default function Homepage(props) {
+    return (
+        <div className="min-h-screen bg-slate-200">
+            <Head title={props.title} />
+            <Navbar />
+            <div className="flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-center p-4 gap-4">
+                <NewsLists news={props.news.data} />
+            </div>
+            <div className="flex justify-center items-center">
+                <Paginator meta={props.news.meta} />
+            </div>
+        </div>
+    );
+}
